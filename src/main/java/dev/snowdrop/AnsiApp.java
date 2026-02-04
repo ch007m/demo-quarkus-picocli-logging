083867d19da1cf57d4add9ba05a326930fea41ae
Charles Moulliard <cmoulliard@ibm.com>
2026-02-04 09:53:32 +0100

    Split the code using jboss formula from aesh ansibuilder
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- src/main/java/dev/snowdrop/AnsiApp.java
+++ src/main/java/dev/snowdrop/AnsiApp.java
@@ -10,38 +10,10 @@ import org.aesh.terminal.utils.TerminalColorCapability;
 
 import java.io.IOException;
 
-import static java.lang.Math.abs;
-
 public class AnsiApp {
-    static int MODE = 39;
-
-    static int FATAL_LEVEL = 1100;
-    static int ERROR_LEVEL = 1000;
-    static int WARN_LEVEL = 900;
-    static int INFO_LEVEL = 800;
-    static int DEBUG_LEVEL = 500;
-    static int TRACE_LEVEL = 400;
-
-    static int LARGEST_LEVEL = ERROR_LEVEL;
-    static int SMALLEST_LEVEL = TRACE_LEVEL;
-    static int SATURATION = 66;
-
-    static int darken;
-    static int r;
-    static int g;
-    static int b;
 
     public static void main(String[] args) {
         try {
-            darken = 0;
-            System.out.printf("Printing different log messages using darken: %d\n", darken);
-            printMessages();
-            System.out.println("####################");
-
-            darken = 1;
-            System.out.printf("Printing different log messages using darken: %d\n", darken);
-            printMessages();
-            System.out.println("####################");
 
             TerminalConnection connection = new TerminalConnection();
             // Using the default color code and check if the theme is dark or light
@@ -162,62 +134,5 @@ public class AnsiApp {
             System.err.println("Error creating terminal connection: " + e.getMessage());
             System.exit(1);
         }
-    }
-
-    static StringBuilder startColor(StringBuilder target, int mode, boolean trueColor, int r, int g, int b) {
-        return target.appendCodePoint(27).append('[').append(mode).append(';').append(2).append(';').append(clip(r))
-            .append(';').append(clip(g)).append(';').append(clip(b)).append('m');
-    }
-
-    static StringBuilder endColor(StringBuilder target, int mode) {
-        return target.appendCodePoint(27).append('[').append(mode).append('m');
-    }
-
-    static int clip(int color) {
-        return Math.min(Math.max(0, color), 255);
-    }
-
-    static void calculateRGB(int LEVEL) {
-        final int level = Math.max(Math.min(LEVEL, LARGEST_LEVEL), SMALLEST_LEVEL) - SMALLEST_LEVEL;
-        r = ((level < 300 ? 0 : (level - 300) * (255 - SATURATION) / 300) + SATURATION) >>> darken;
-        g = ((300 - abs(level - 300)) * (255 - SATURATION) / 300 + SATURATION) >>> darken;
-        b = ((level > 300 ? 0 : level * (255 - SATURATION) / 300) + SATURATION) >>> darken;
-    }
-
-    static void printMessages() {
-        StringBuilder target = new StringBuilder();
-        calculateRGB(INFO_LEVEL);
-        startColor(target, 38, true, r,g,b);
-        target.append("INFO log message");
-        endColor(target, MODE);
-        System.out.printf("%s\n", target);
-
-        target = new StringBuilder();
-        calculateRGB(WARN_LEVEL);
-        startColor(target, 38, true, r,g,b);
-        target.append("WARN log message");
-        endColor(target, MODE);
-        System.out.printf("%s\n", target);
-
-        target = new StringBuilder();
-        calculateRGB(ERROR_LEVEL);
-        startColor(target, 38, true, r,g,b);
-        target.append("ERROR log message");
-        endColor(target, MODE);
-        System.out.printf("%s\n", target);
-
-        target = new StringBuilder();
-        calculateRGB(DEBUG_LEVEL);
-        startColor(target, 38, true, r,g,b);
-        target.append("DEBUG log message");
-        endColor(target, MODE);
-        System.out.printf("%s\n", target);
-
-        target = new StringBuilder();
-        calculateRGB(TRACE_LEVEL);
-        startColor(target, 38, true, r,g,b);
-        target.append("TRACE log message");
-        endColor(target, MODE);
-        System.out.printf("%s\n", target);
     }
 }
---
+++ src/main/java/dev/snowdrop/JBossLoggingApp.java
@@ -0,0 +1,85 @@
+/// usr/bin/env jbang “$0” “$@” ; exit $?
+//DEPS org.aesh:terminal-tty:3.0-dev
+
+package dev.snowdrop;
+
+import org.aesh.terminal.tty.TerminalConnection;
+import org.aesh.terminal.utils.ANSIBuilder;
+import org.aesh.terminal.utils.ColorDepth;
+import org.aesh.terminal.utils.TerminalColorCapability;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static java.lang.Math.abs;
+
+public class JBossLoggingApp {
+    static int MODE = 39;
+
+    static int FATAL_LEVEL = 1100;
+    static int ERROR_LEVEL = 1000;
+    static int WARN_LEVEL = 900;
+    static int INFO_LEVEL = 800;
+    static int DEBUG_LEVEL = 500;
+    static int TRACE_LEVEL = 400;
+
+    static int LARGEST_LEVEL = ERROR_LEVEL;
+    static int SMALLEST_LEVEL = TRACE_LEVEL;
+    static int SATURATION = 66;
+
+    static int darken;
+    static int r;
+    static int g;
+    static int b;
+
+    public static void main(String[] args) {
+        try {
+            for (int num: Arrays.asList(0,1)) {
+                darken = num;
+                System.out.printf("The jboss way to calculate RGB colors for messages to log using darken: %d\n", darken);
+                printMessages();
+                System.out.println("####################");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void printMessages() throws NoSuchFieldException, IllegalAccessException {
+        for (String LEVEL: Arrays.asList("TRACE","DEBUG","INFO","WARN","ERROR", "FATAL")) {
+            StringBuilder target = new StringBuilder();
+
+            calculateRGB((int) JBossLoggingApp.class.getDeclaredField(LEVEL + "_LEVEL").get(null));
+
+            startColor(target, 38, true, r,g,b);
+            target.append(LEVEL);
+            endColor(target, MODE);
+
+            startColor(target, 38, true, 176, 208, 176);
+            target.append(" log message.");
+            endColor(target, MODE);
+
+            System.out.printf("%s\n", target);
+        }
+    }
+
+    static StringBuilder startColor(StringBuilder target, int mode, boolean trueColor, int r, int g, int b) {
+        return target.appendCodePoint(27).append('[').append(mode).append(';').append(2).append(';').append(clip(r))
+            .append(';').append(clip(g)).append(';').append(clip(b)).append('m');
+    }
+
+    static StringBuilder endColor(StringBuilder target, int mode) {
+        return target.appendCodePoint(27).append('[').append(mode).append('m');
+    }
+
+    static int clip(int color) {
+        return Math.min(Math.max(0, color), 255);
+    }
+
+    static void calculateRGB(int LEVEL) {
+        final int level = Math.max(Math.min(LEVEL, LARGEST_LEVEL), SMALLEST_LEVEL) - SMALLEST_LEVEL;
+        r = ((level < 300 ? 0 : (level - 300) * (255 - SATURATION) / 300) + SATURATION) >>> darken;
+        g = ((300 - abs(level - 300)) * (255 - SATURATION) / 300 + SATURATION) >>> darken;
+        b = ((level > 300 ? 0 : level * (255 - SATURATION) / 300) + SATURATION) >>> darken;
+    }
+}
