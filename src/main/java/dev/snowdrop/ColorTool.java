c1cc009863c9d8aaf9cd4f920147e74ea51cb631
Charles Moulliard <cmoulliard@ibm.com>
2026-02-05 07:41:35 +0100

    Enhance color code including message
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- src/main/java/dev/snowdrop/ColorTool.java
+++ src/main/java/dev/snowdrop/ColorTool.java
@@ -28,7 +28,7 @@ public class ColorTool {
 
         render("FATAL", 0, theme);
         render("ERROR", 10, theme);
-        render("WARN ", 35, theme);
+        render("WARN ", 40, theme);
         render("INFO ", 100, theme);
         render("DEBUG", 180, theme);
         render("TRACE", 220, theme);
@@ -36,31 +36,39 @@ public class ColorTool {
     }
 
     static void render(String level, int hueValue, Theme theme) {
-        float h, s, v;
         var logMsg = "This is a log message. The application has well started and is running on port: 8080.";
-        h = hueValue;
 
-        if (theme == Theme.DARK) {
-            s = 0.75f; // If we decrease the saturation, then the colors become less vivid
-            v = 1.0f;
-        } else {
-            s = (hueValue > 200) ? 0.60f : 0.75f;
-            v = 0.85f;
-        }
-
-        int rgb = Color.HSBtoRGB(h / 360f, s, v);
-        Color c = new Color(rgb);
-
-        // Dark Theme: Muted Sage (174, 217, 185)
-        // Light Theme: Deep Slate Green (60, 90, 70) for contrast
-        String msgColor = (theme == Theme.DARK)
-            ? "\u001B[38;2;174;217;185m"
-            : "\u001B[38;2;179;201;187m";
+        Color c = calculateColor(hueValue, theme);
 
         String startColor = String.format("\u001B[38;2;%d;%d;%dm", c.getRed(), c.getGreen(), c.getBlue());
         String reset = "\u001B[0m";
 
-        System.out.printf("[%3d] %s%-6s%s - RGB: (%3d, %3d, %3d) - H:%.0f S:%.0f%% V:%.0f%% - %s%s%s%n",
-            hueValue, startColor, level, reset, c.getRed(), c.getGreen(), c.getBlue(), h, s * 100, v * 100, msgColor, logMsg, reset);
+        float h, s, v;
+        h = 140f;
+        s = 0.10f;
+        v = 0.58f;
+        int rgb = Color.HSBtoRGB(h, s, v);
+        c = new Color(rgb);
+        String msgColor = String.format("\u001B[38;2;%d;%d;%dm", c.getRed(), c.getGreen(), c.getBlue());;
+
+        System.out.printf("%s%-6s%s %s%s%s%n",
+            startColor, level, reset, msgColor, logMsg, reset);
+    }
+
+    static Color calculateColor(int h, Theme theme) {
+        float s, v;
+        if (theme == Theme.DARK) {
+            s = 0.75f; // If we decrease the saturation, then the colors become less vivid
+            v = 1.0f;
+        } else {
+            s = (h > 200) ? 0.60f : 0.75f;
+            v = 0.70f;
+        }
+
+        int rgb = Color.HSBtoRGB(h / 360f, s, v);
+        Color c = new Color(rgb);
+        //System.out.printf("RGB: (%3d, %3d, %3d) - H:%.0f S:%.0f%% V:%.0f%%\n",
+        //    c.getRed(), c.getGreen(), c.getBlue(), (float)h, s * 100, v * 100);
+        return c;
     }
 }
